ef5821ec592bc57c88cfe60124d3897eef10b113
Cassio Alves <cassiocafs@gmail.com>
2014-03-20 20:57:53 -0300

    Criando o projeto TreinaDog

---
+++ src/main/java/br/com/treina/dog/controlador/Mundo.java
@@ -0,0 +1,13 @@
+package br.com.treina.dog.controlador;
+
+import br.com.caelum.vraptor.Resource;
+
+@Resource
+public class Mundo {
+
+	public String olaMundo(){
+		return "treina dog";
+	}
+	
+	
+}
